74124e6338fca38eb1c2ba0b41833ce20a77d49d
Unknown Developer <138309645+Shivam230708@users.noreply.github.com>
2025-03-25 15:38:12 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,3 +1,14 @@
+from pyrogram import Client
+
+app = Client(
+    "my_account",
+    api_id=int(os.getenv("API_ID")),
+    api_hash=os.getenv("API_HASH"),
+    bot_token=os.getenv("BOT_TOKEN")
+)
+
+app.run()
+
 import time
 import datetime
 import os
